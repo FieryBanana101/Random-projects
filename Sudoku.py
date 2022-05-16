6b24f9457fd41e3a4489ac0946a3743653cb31be
FieryBanana101 <105137802+FieryBanana101@users.noreply.github.com>
2022-05-16 12:18:06 +0700

    Added solution count index
    Added solution index so before showing solved sudoku, the index will be showed to user (Index are: Solution 1, Solution 2, Solution 3, etc), Also cleaned up the code a little bit bye changing some variable placement, removed unnecessary variable assignment, changed some comment placement

--- Sudoku.py
+++ Sudoku.py
@@ -1,37 +1,41 @@
-
 sudoku = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 8, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 9]
-
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
 ]
+solution_c = 1
 
 # Print the sudoku with special reformatting
 def print_result(data):
-    for k in range(0,7,3):
+    global solution_c
+    print(f"Solution {solution_c}")
+    for k in range(0, 7, 3):
         for i in range(3):
-            print(data[i+k][0:3],"|",data[i+k][3:6],"|",data[i+k][6:9])
-        print("-"*34)
+            print(data[i + k][0:3], "|", data[i + k][3:6], "|", data[i + k][6:9])
+        print("- " * 34)
     print("-" * 100)
+    solution_c += 1
+
 
 # Find the position in coordinate y,x for the next empty box, start from (0,0) and end at (8,8)
 # Iterate for each row first
-def find_empty(data:list):
+def find_empty(data: list):
     for y in range(len(data)):
         for x in range(len(data[y])):
             if data[y][x] == 0:
                 return y, x
     return None
 
+
 # Return False or True based on whether can we put the number "num" inside a given coordinate position (y,x)
 # inside the sudoku, evaluated using the regular sudoku rules
-def isPossible(data:list, num:int, y:int, x:int):
+def isPossible(data: list, num: int, y: int, x: int):
     for i in range(len(data[y])):
         if num == data[y][i]:
             return False
@@ -48,9 +52,10 @@ def isPossible(data:list, num:int, y:int, x:int):
                 return False
     return True
 
+
 # solve the sudoku using previous function using brute force by iterating through all element, and backtracking
 # if we get stumped on a certain position
-def solve(data:list):
+def solve(data: list):
     try:
         if find_empty(data) is not None:
             y, x = find_empty(data)
@@ -62,13 +67,16 @@ def solve(data:list):
     except UnboundLocalError:
         print_result(data)
     # UnboundLocalError will only happen if sudoku is solved (happen because y,x is unassigned),
-    # so if sudoku is solved print the result
-    # Probably need a more optimal way for the recursion, This is good for now
 
+
+# so if sudoku is solved print the result
+# this is the first time i ever encounter an error which instead give me an advantage lol
+# I have a lot of problem doing the recursion, so this is the best i've got, idk if it's optimal
 
 i = 0
+
 while i < 9:
-    sudoku[i] = input("Row "+str(i+1)+": ").split(" ")
+    sudoku[i] = input("Row  " + str(i + 1) + ": ").split(" ")
     if sudoku[i][-1] == "":
         sudoku[i].pop(-1)
     if len(sudoku[i]) != 9:
@@ -78,9 +86,10 @@ while i < 9:
         if 0 > int(sudoku[i][j]) or int(sudoku[i][j]) > 9:
             print("invalid input, please enter number from 0-9")
             i -= 1
+    for k in range(9):
+        sudoku[i][k] = int(sudoku[i][k])
     i += 1
 
 solve(sudoku)
 
 # Everything should be done, but probably need optimization
-
